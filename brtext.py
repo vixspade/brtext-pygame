927781b690384d1e59551ccf790a2b6111392e77
Vix Spade <51852802+vixspade@users.noreply.github.com>
2024-04-06 10:17:20 +0300

    comments
    i love reading

--- brtext.py
+++ brtext.py
@@ -1,4 +1,3 @@
-# IMPORT
 import pygame
 from pygame.locals import *
 import random
@@ -11,31 +10,31 @@ h = 880
 size = 48
 lineoff = (size / 2) + 10
 dt = time.perf_counter()
+line1 = "NOW THE SHOW IS ALL YOURS ."   # ONLY EDIT 
+line2 = "WHAT DO YOU HAVE TO SAY ?"     # line1 and line2!
+text1 = ''
+text2 = ''
 
-# INIT PYGAME + FONTS
+# INIT PYGAME + FONT
 pygame.init()
 screen = pygame.display.set_mode((w, h))
 font = pygame.font.SysFont('fakereceiptregular', size)
 
-line1 = "NOW THE SHOW IS ALL YOURS ."
-line2 = "WHAT DO YOU HAVE TO SAY ?"
-text1 = ''
-text2 = ''
-# START GAME
+# START
 running = True
 input()
 while running:
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
-    if time.perf_counter() - dt >= 0.03:
+    if time.perf_counter() - dt >= 0.03:  # Add letters if 0.03s has passed
         if len(text1) < len(line1):
-            text1 += line1[len(text1)]
-        elif len(text2) < len(line2):
+            text1 += line1[len(text1)]    # If line 1 is incomplete, add to it,
+        elif len(text2) < len(line2):     # otherwise add to line 2
             text2 += line2[len(text2)]
         dt = time.perf_counter()
     offsetx = random.randint(0, 2) - 1
-    offsety = random.randint(0, 2) - 1
+    offsety = random.randint(0, 2) - 1    # Random x & y offset for text
     screen.fill((0, 0, 0))
     img1 = font.render(text1, False, colour)
     img2 = font.render(text2, False, colour)
